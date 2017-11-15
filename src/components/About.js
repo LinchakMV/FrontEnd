474cb6da206997582af22ceabac0e62c0a46d803
linchakmv <linchakmv@gmail.com>
2017-11-16 00:46:40 +0200

    delete comments

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,6 @@ import React from 'react';
 import photo from '../img/photo.jpg';
 
 function About() {
-  // const time  = ['00:00', '03:00', '06:00','09:00','12:00','15:00','18:00','21:00'];
-  // const timeRow = time.map((hour,index) => <div key={index} className='time-cell'>{hour}</div>);
   return (
     <section className="about">
       <a name="About" />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 function Header() {
-  // const time  = ['00:00', '03:00', '06:00','09:00','12:00','15:00','18:00','21:00'];
-  // const timeRow = time.map((hour,index) => <div key={index} className='time-cell'>{hour}</div>);
   return (
     <section className="home_wrap">
       <div className="home_menu">
